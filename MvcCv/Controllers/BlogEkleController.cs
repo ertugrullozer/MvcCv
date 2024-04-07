9231b567c58188e5d5cf8ee3e8c31e8c34caebb9
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-07 16:25:45 +0300

    Resim Yükleme Metot Yazıldı

--- MvcCv/Controllers/BlogEkleController.cs
+++ MvcCv/Controllers/BlogEkleController.cs
@@ -6,20 +6,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcCv.Models.sınıfım;
+using System.IO;
 
 namespace MvcCv.Controllers
 {
     public class BlogEkleController : Controller
     {
-        Card1Repository  repo = new Card1Repository();
+        Card1Repository repo = new Card1Repository();
         Card2Repository repo2 = new Card2Repository();
-        
+
         public ActionResult Index()
-        { Class1 class1 = new Class1();
+        {
+            Class1 class1 = new Class1();
             class1.tBLblogCard1s = repo.List();
-            class1.tBLblogCard2s=repo2.List();
-           
-          return View(class1);
+            class1.tBLblogCard2s = repo2.List();
+
+            return View(class1);
         }
         [HttpGet]
         public ActionResult Card1Ekle()
@@ -27,29 +29,39 @@ namespace MvcCv.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Card1Ekle(TBLblogCard1 p)
+        public ActionResult Card1Ekle([Bind(Include = "ID,Card1Resim,Card1Baslik,Card1AltBaslik,Card1Tarih,Card1Aciklama")] TBLblogCard1 p, HttpPostedFileBase yüklenecekDosya)
         {
+           
+            if (yüklenecekDosya !=null)
+            {
+                string dosyaYolu = Path.GetFileName(yüklenecekDosya.FileName);
+                var yüklemeYeri = Path.Combine(Server.MapPath("~/BlogImage"), dosyaYolu);
+                yüklenecekDosya.SaveAs(yüklemeYeri);
+                p.Card1Resim = "/BlogImage/" + dosyaYolu;
+              
+            }
+
             repo.TAdd(p);
             return RedirectToAction("Index");
         }
         [HttpGet]
-        public ActionResult Card2Ekle() 
+        public ActionResult Card2Ekle()
         {
             return View();
         }
         [HttpPost]
         public ActionResult Card2Ekle(TBLblogCard2 p)
         {
-           repo2.TAdd(p);
+            repo2.TAdd(p);
             return RedirectToAction("Index");
         }
         public ActionResult Card1Sil(int id)
         {
             TBLblogCard1 t = repo.Find(x => x.ID == id);
-           repo.TDelete(t);
+            repo.TDelete(t);
             return RedirectToAction("Index");
         }
-        public ActionResult Card2Sil(int id) 
+        public ActionResult Card2Sil(int id)
         {
             TBLblogCard2 t = repo2.Find(x => x.ID == id);
             repo2.TDelete(t);
@@ -58,13 +70,13 @@ namespace MvcCv.Controllers
         [HttpGet]
         public ActionResult Card1Güncelle(int id)
         {
-            TBLblogCard1 t= repo.Find(x => x.ID == id);
+            TBLblogCard1 t = repo.Find(x => x.ID == id);
             return View(t);
         }
         [HttpPost]
-        public ActionResult Card1Güncelle(TBLblogCard1 p) 
+        public ActionResult Card1Güncelle(TBLblogCard1 p)
         {
-            TBLblogCard1 t= repo.Find(x=>x.ID == p.ID);
+            TBLblogCard1 t = repo.Find(x => x.ID == p.ID);
             t.Card1Resim = p.Card1Resim;
             t.Card1Baslik = p.Card1Baslik;
             t.Card1AtBaslik = p.Card1AtBaslik;
@@ -77,15 +89,15 @@ namespace MvcCv.Controllers
         [HttpGet]
         public ActionResult Card2Güncelle(int id)
         {
-            TBLblogCard2 t = repo2.Find(x=>x.ID == id);
+            TBLblogCard2 t = repo2.Find(x => x.ID == id);
 
             return View(t);
         }
         [HttpPost]
         public ActionResult Card2Güncelle(TBLblogCard2 p)
         {
-            TBLblogCard2 t= repo2.Find(x=>x.ID == p.ID);
-            t.Baslik= p.Baslik;
+            TBLblogCard2 t = repo2.Find(x => x.ID == p.ID);
+            t.Baslik = p.Baslik;
             t.Aciklama = p.Aciklama;
             repo2.TUpdate(t);
             return RedirectToAction("Index");
